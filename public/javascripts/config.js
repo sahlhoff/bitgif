7b11231997c65077c2b641dc5e1aa78c5a28153e
Chad Sahlhoff <csahlhoff@gmail.com>
2014-06-10 00:04:49 -0700

    added readmen

--- public/javascripts/config.js
+++ public/javascripts/config.js
@@ -3,11 +3,15 @@ requirejs.config({
   paths: {
     'jquery': 'lib/jquery/dist/jquery',
     'bitcore': 'lib/bitcore/browser/bundle',
-    'qrcode': 'lib/qrcode/qrcode'
+    'qrcode': 'lib/qrcode/qrcode',
+    'Animated_GIF': 'lib/Animated_GIF/dist/Animated_GIF.min'
   },
   shim: {
     'jquery': {
       exports: 'jQuery'
+    },
+    'qrcode': {
+      exports: 'qrcode'
     }
   }
 });
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,33 +1,52 @@
-define(['jquery', 'bitcore', 'qrcode'],
-  function ($, bitcore, qrcode) {
+define(['jquery', 'bitcore', 'qrcode', 'Animated_GIF'],
+  function ($, bitcore, qrcode, Animated_GIF) {
   'use strict';
 
+  var keyImage;
+  var addressImage;
   var bitcore = require('bitcore');
   var Key = bitcore.Key;
   var Address = bitcore.Address;
-  var re = /[0-9]{6}$/;
-  var a,k,m;
+  var interval = 0.1;
+
+  var ag = new Animated_GIF({ 
+    workerPath: 'javascripts/lib/animated_GIF/dist/Animated_GIF.worker.min.js' 
+  }); 
+  ag.setSize(320, 240);
 
   var generateAddress = function (k){
     var a;
     a = Address.fromKey(k);
-
     return a;
   }
 
   var generateKey = function (){
     var k;
     k = Key.generateSync();
-
     return k;
   }
 
-  var address = generateAddress();
+  var create_qrcode = function (text, typeNumber, errorCorrectLevel, table){
+    var qr = qrcode(typeNumber || 4, errorCorrectLevel || 'M')
+    qr.addData(text);
+    qr.make();
 
-  console.log(address);
+    return qr.createImgTag();
+  }
 
-  console.log('Address: ' + address[0].toString());
-  console.log('Private Key: ' + address[1].private.toString('hex'));
+  var key = generateKey();
+  var address = generateAddress(key);
+
+  addressImage = create_qrcode(address.toString());
+  keyImage = create_qrcode(key.private.toString('hex'));
+
+  ag.addFrame(addressImage);
+  ag.addFrame(keyImage);
+
+  ag.getBase64GIF(function(image) {
+      animatedImage.src = image;
+      document.body.appendChild(animatedImage);
+  });
 
 
 });
