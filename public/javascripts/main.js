7c7ee13a318437fc82ffbbb88e4dd1ac48d58ec7
Chad Sahlhoff <csahlhoff@gmail.com>
2014-06-10 23:23:07 -0700

    added qr functionality

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -7,12 +7,17 @@ define(['jquery', 'bitcore', 'qrcode', 'Animated_GIF'],
   var bitcore = require('bitcore');
   var Key = bitcore.Key;
   var Address = bitcore.Address;
+  var animatedImage = document.createElement('img');
+  var addressObject = document.createElement('div');
+  var keyObject = document.createElement('div');
   var interval = 0.1;
+
 
   var ag = new Animated_GIF({ 
     workerPath: 'javascripts/lib/animated_GIF/dist/Animated_GIF.worker.min.js' 
   }); 
-  ag.setSize(320, 240);
+  ag.setSize(320, 340);
+
 
   var generateAddress = function (k){
     var a;
@@ -27,22 +32,30 @@ define(['jquery', 'bitcore', 'qrcode', 'Animated_GIF'],
   }
 
   var create_qrcode = function (text, typeNumber, errorCorrectLevel, table){
-    var qr = qrcode(typeNumber || 4, errorCorrectLevel || 'M')
+    var qr = qrcode(typeNumber || 6, errorCorrectLevel || 'M')
     qr.addData(text);
     qr.make();
 
     return qr.createImgTag();
   }
 
+  // Generate key and address
   var key = generateKey();
   var address = generateAddress(key);
 
+  // Create strings
   addressImage = create_qrcode(address.toString());
   keyImage = create_qrcode(key.private.toString('hex'));
 
-  ag.addFrame(addressImage);
-  ag.addFrame(keyImage);
+  // Create Dom objects
+  addressObject.innerHTML = addressImage;
+  keyObject.innerHTML = keyImage;
 
+  // Add images to frame
+  ag.addFrame(addressObject.firstChild);
+  ag.addFrame(keyObject.firstChild);
+
+  // Generate gif
   ag.getBase64GIF(function(image) {
       animatedImage.src = image;
       document.body.appendChild(animatedImage);
